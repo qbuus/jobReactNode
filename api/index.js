88cd311b4bf505e1395bcaa0c1f76709d2bb7f00
qbuus <jkocz867@gmail.com>
2023-09-11 14:43:42 +0200

    api view 404, default view

--- api/index.js
+++ api/index.js
@@ -1,10 +1,29 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import rootRouter from "./routes/main.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const SERVER_PORT = process.env.PORT || 8010;
 
 const app = express();
+
+app.use("/", rootRouter);
+
+app.use("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "notfound.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on port: ${SERVER_PORT}`);
