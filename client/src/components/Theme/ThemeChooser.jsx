ea38e039a2471b268ff0bf0492167683342dae42
qbuus <jkocz867@gmail.com>
2023-09-20 11:00:01 +0200

    theme chooser style change

--- client/src/components/Theme/ThemeChooser.jsx
+++ client/src/components/Theme/ThemeChooser.jsx
@@ -9,17 +9,19 @@ const ThemeChooser = () => {
 
   return (
     <>
-      {themeData
-        ? themeData.map((theme, index) => (
-            <li
-              data-set-theme={`${theme.theme}`}
-              className="ACTIVECLASS"
-              key={index}
-            >
-              {theme.theme}
-            </li>
-          ))
-        : null}
+      <div className="flex flex-col gap-3">
+        {themeData
+          ? themeData.map((theme, index) => (
+              <li
+                data-set-theme={`${theme.theme}`}
+                className="font-normal text-lg hover:bg-base-100 w-full rounded-md"
+                key={index}
+              >
+                {theme.theme}
+              </li>
+            ))
+          : null}
+      </div>
     </>
   );
 };
